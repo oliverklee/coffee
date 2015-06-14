4ce8b6fd818b80e5ce9f22cb2b8ebabb8ea79f92
Oliver Klee <typo3-coding@oliverklee.de>
2015-06-14 12:53:18 +0200

    [FEATURE] Add a CoffeeBeverageModel

---
+++ Classes/Domain/Model/CoffeeBeverage.php
@@ -0,0 +1,23 @@
+<?php
+namespace OliverKlee\Coffee\Domain\Model;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * This model represents a coffee beverage.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class CoffeeBeverage extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+}
---
+++ Tests/Unit/Domain/Model/CoffeeBeverageTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace OliverKlee\Coffee\Tests;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Test case.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class CoffeeBeverageTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+	/**
+	 * @var \OliverKlee\Coffee\Domain\Model\CoffeeBeverage
+	 */
+	protected $subject = NULL;
+
+	protected function setUp() {
+		$this->subject = new \OliverKlee\Coffee\Domain\Model\CoffeeBeverage();
+	}
+
+	/**
+	 * @test
+	 */
+	public function canBeInstantiated() {
+		self::assertInstanceOf('OliverKlee\\Coffee\\Domain\\Model\\CoffeeBeverage', $this->subject);
+	}
+}
