94ae35220b2929bd03c8f1687e7984eaeb8d4b22
Oliver Klee <typo3-coding@oliverklee.de>
2015-06-14 13:04:28 +0200

    [FEATURE] Add a WorldTest

---
+++ Tests/Unit/WorldTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace OliverKlee\Coffee\Tests\Unit;
+
+class CoffeeBeverageTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+	/**
+	 * @test
+	 */
+	public function universeHasSpaceTimeContinuum() {
+		self::assertSame(
+			4,
+			2 + 2
+		);
+	}
+}
