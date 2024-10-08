c5ef87b45fbee153c961785925b33b693dd6e79d
Oliver Klee <typo3-coding@oliverklee.de>
2024-10-08 18:42:02 +0200

    [FEATURE] Add Rector
    Fixes #238

---
+++ rector.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/Classes/',
+        __DIR__ . '/Tests/',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_81)
+    ->withPhpSets(true)
+    // Note: We're only enabling a single set by default to improve performance. (Rector needs at least a single set to
+    // run.)
+    //
+    // You can temporarily enable more sets as needed.
+    ->withSets([
+        // Rector sets
+
+        // LevelSetList::UP_TO_PHP_53,
+        // LevelSetList::UP_TO_PHP_54,
+        // LevelSetList::UP_TO_PHP_55,
+        // LevelSetList::UP_TO_PHP_56,
+        // LevelSetList::UP_TO_PHP_70,
+        // LevelSetList::UP_TO_PHP_71,
+        // LevelSetList::UP_TO_PHP_72,
+        // LevelSetList::UP_TO_PHP_73,
+        // LevelSetList::UP_TO_PHP_74,
+        // LevelSetList::UP_TO_PHP_80,
+        LevelSetList::UP_TO_PHP_81,
+        // LevelSetList::UP_TO_PHP_82,
+        // LevelSetList::UP_TO_PHP_83,
+
+        // SetList::CODE_QUALITY,
+        // SetList::CODING_STYLE,
+        // SetList::DEAD_CODE,
+        // SetList::EARLY_RETURN,
+        // SetList::INSTANCEOF,
+        // SetList::NAMING,
+        // SetList::PRIVATIZATION,
+        // SetList::STRICT_BOOLEANS,
+        // SetList::TYPE_DECLARATION,
+
+        // PHPUnit sets
+
+        // PHPUnitSetList::PHPUNIT80_DMS,
+        // PHPUnitSetList::PHPUNIT_40,
+        // PHPUnitSetList::PHPUNIT_50,
+        // PHPUnitSetList::PHPUNIT_60,
+        // PHPUnitSetList::PHPUNIT_70,
+        // PHPUnitSetList::PHPUNIT_80,
+        // PHPUnitSetList::PHPUNIT_90,
+        // PHPUnitSetList::PHPUNIT_100,
+        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    ]);
