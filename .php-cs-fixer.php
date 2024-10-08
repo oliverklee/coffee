0b4c3f8e73b8140123b2d8da65baadc332178797
Oliver Klee <typo3-coding@oliverklee.de>
2024-10-08 18:54:29 +0200

    [TASK] Switch to a Symfony-style directory structure
    Fixes #248

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
-$config->getFinder()->in(__DIR__ . '/Classes')->in(__DIR__ . '/Tests');
+$config->getFinder()->in(__DIR__ . '/src')->in(__DIR__ . '/tests');
 
 return $config;
